327c35ad5f6a241c1a253e3c74cdd81e29c1a210
JonasJ-ap <rushanj@andrew.cmu.edu>
2022-10-19 16:40:56 -0400

    add app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from distutils.command.config import config
+import yaml
+import argparse
+from rosproject.rosproject import Project
+from rostasks.csvtask import CsvTask
+from rostasks.pointcloudtask import PointcloudTask
+
+
+def get_properties(filename):
+    with open(filename) as f:
+        properties = yaml.load(f, Loader=yaml.FullLoader)
+    return properties
+
+
+def get_tasks(properties):
+    tasks = []
+    if properties['csv_task'] == 'true':
+        tasks.append(CsvTask())
+    if properties['pointcloud_task'] == 'true':
+        tasks.append(PointcloudTask())
+    return tasks
+
+
+def main(config_file):
+    properties = get_properties(config_file)
+    rosproject = Project(properties=properties)
+    rosproject.processAllRuns(get_tasks(properties))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ROS dataset processor")
+    parser.add_argument("-c", "--config", help="Config file")
+    args = parser.parse_args()
+    if args.config:
+        main(args.config)
+    else:
+        parser.print_help()
--- rosproject/rosproject.py
+++ rosproject/rosproject.py
@@ -1,18 +1,30 @@
 import os
-from typing import List
+from typing import List, Dict
 from rostasks import Rostask
+
+
+PROJECT_PATH = "project_path"
+RUN_TO_BAGS = "run_to_bags"
+PROJECT_NAME = "project_name"
 
 
 class Project:
 
-    def __init__(self, project_path, project_name, run_to_bags, properties):
-        self.project_path = project_path
-        self.project_name = project_name
-        self.run_to_bags = run_to_bags
+    def __init__(self, properties: Dict[str, str]):
+        self.properties = properties
+        self.project_path = properties.get(PROJECT_PATH, "")
+        self.run_to_bags = properties.get(RUN_TO_BAGS, "")
+        self.project_name = properties.get(PROJECT_NAME, "default")
+        if not self.validate():
+            raise Exception("Invalid project properties")
         self.run_names = [runName for runName in os.listdir(
             self.project_path) if os.path.isdir(os.path.join(self.project_path, runName))]
         self.processedRuns = []
-        self.properties = properties
+
+    def validate(self):
+        if self.project_path == "" or self.run_to_bags == "" or self.project_name == "":
+            return False
+        return True
 
     def getRunPaths(self):
         return self.runPaths
